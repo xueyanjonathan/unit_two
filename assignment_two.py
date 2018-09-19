b8f6dcc45fd568f9d4cc50cd6af5123cf59ebf79
Jonathan <jonathan@iMac9.local>
2018-09-19 08:54:36 -0400

    Use input to start to write the chatbox

---
+++ assignment_two.py
@@ -0,0 +1,15 @@
+#Get the user's name
+name = input("What is your name?")
+#Greet the user
+print("Hello, my name is CB, ChatBox not CollegeBoard. " + name + ", it is my pleasure to meet you." )
+#Ask where the user is from
+place = input(name + ", where are you from?")
+#Reply the user by praising the place
+print("Oh, " + place + ", it is a beautiful place. I wish I can visit there someday, but I can only stay in this computer." )
+#Get the user's age
+age = input("How old are you, " + name + "?")
+#Write down the formula of the ChatBox's madeup age(100 years old) divded by the user's age
+ageoverage = 100 / float(age)
+#Reply with the relationship between the age
+print("Funny, I am", ageoverage, "times older than you.")
+#Find out the type of the user's dreamhouse
