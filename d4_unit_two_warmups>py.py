a76ba2a2844b8838cd6c44e50354455e2e85c31f
Jonathan <jonathan@iMac9.local>
2018-09-20 09:15:39 -0400

    Mathmatical operation

---
+++ d4_unit_two_warmups>py.py
@@ -0,0 +1,11 @@
+print(9 * 5)
+print(2 / 5)
+print(12 // 5)
+print(27 //4)
+print(2 // 5)
+print(5 % 2)
+print(9 % 5)
+print(6 % 6)
+print(2 % 7)
+# // means that the first number is doing interger division to the second number, without considering remainder in the equation.
+# % means the remainder after the integer division
